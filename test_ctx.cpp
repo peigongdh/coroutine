ea7df376c05284d5a8d2699681adec65670499d2
zhangpei <zhangpei@qutoutiao.net>
2020-07-27 17:35:49 +0800

    import coroutine&context repository

---
+++ test_ctx.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "contextu.h"
+
+
+int main(void) {
+    ctx_context_t r;
+    int hr;
+    volatile int mode = 0;
+
+    hr = ctx_getcontext(&r);
+    printf("ctx_getcontext() -> %d\n", hr);
+
+    if (mode == 0) {
+        mode++;
+        printf("first run\n");
+        ctx_setcontext(&r);
+    } else {
+        printf("second run\n");
+    }
+    printf("endup\n");
+
+    return 0;
+}
+
+
---
+++ test_swap.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "contextu.h"
+
+
+char temp_stack[32768];
+ctx_context_t mc, cc;
+
+int raw_thread(void *p) {
+    printf("remote: hello %s\n", (char *) p);
+    ctx_swapcontext(&cc, &mc);
+    printf("remote: back again\n");
+    ctx_swapcontext(&cc, &mc);
+    printf("remote: return\n");
+    return 1024;
+}
+
+int main(void) {
+    cc.stack = temp_stack;
+    cc.stack_size = sizeof(temp_stack);
+    cc.link = &mc;
+
+    ctx_getcontext(&cc);
+    ctx_makecontext(&cc, raw_thread, (char *) "girl");
+
+    printf("before switch: %d\n", cc.stack_size);
+    ctx_swapcontext(&mc, &cc);
+
+    printf("local: here\n");
+    ctx_swapcontext(&mc, &cc);
+
+    printf("local: again\n");
+    ctx_swapcontext(&mc, &cc);
+
+    printf("local: end\n");
+    return 0;
+}
+
+
+
